e928e782ca92fee9cc66e3cd1b5d531fb351bd7a
debooger <debooger@debooger.xyz>
2022-07-20 07:32:22 +0200

    Disable -fsanitize=undefined compiler options (UBSAN) by default

--- config.mk
+++ config.mk
@@ -3,7 +3,8 @@ LIBS = -lpthread
 
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O3 -fPIC ${CPPFLAGS} -fsanitize=undefined
+#CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O0 -fPIC ${CPPFLAGS} -fsanitize=undefined
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O3 -fPIC ${CPPFLAGS}
 LDFLAGS  = -shared ${LIBS}
 
 # Compiler and linker
