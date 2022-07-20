36e6715ffee8eba3fe9c48843f1c40f8ef1eb745
debooger <debooger@debooger.xyz>
2022-07-20 07:04:54 +0200

    Initialize in the background
    Use a background thread to prevent game freezing when initializing.

--- ah4.c
+++ ah4.c
@@ -1,9 +1,13 @@
+#include <pthread.h>
+
 #include "interfaces.h"
 #include "memory.h"
 #include "netvars.h"
 #include "hooks.h"
 
-int __attribute__((constructor)) onLoad(void)
+static pthread_t thread;
+
+static void *routine(void *arg)
 {
 	interfaces_init();
 	memory_init();
@@ -13,8 +17,18 @@ int __attribute__((constructor)) onLoad(void)
 	return 0;
 }
 
+int __attribute__((constructor)) onLoad(void)
+{
+	pthread_create(&thread, 0, routine, 0);
+	pthread_detach(thread);
+
+	return 0;
+}
+
 void __attribute__((destructor)) onUnload(void)
 {
+	pthread_join(thread, 0);
+
 	hooks_cleanUp();
 	netvars_cleanUp();
 }
--- config.mk
+++ config.mk
@@ -1,7 +1,10 @@
+# Libraries
+LIBS = -lpthread
+
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O3 -fPIC ${CPPFLAGS} -fsanitize=undefined
-LDFLAGS  = -shared
+LDFLAGS  = -shared ${LIBS}
 
 # Compiler and linker
 CC = cc
