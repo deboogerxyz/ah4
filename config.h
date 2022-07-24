7c46de5b200b93f235867c8ba5a84bb1a7b0022c
debooger <debooger@debooger.xyz>
2022-07-24 12:54:19 +0200

    Added READ_BOOL macro for cJSON

--- config.h
+++ config.h
@@ -1,1 +1,6 @@
+#define READ_BOOL(json, name, var) \
+	{ cJSON *item = cJSON_GetObjectItem(json, name); \
+	if (cJSON_IsBool(item)) \
+		var = item->valueint; }
+
 void config_load(const char *name);
