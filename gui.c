0b39a4a1071d9144225a78fa02a95c8898ac75e8
debooger <debooger@debooger.xyz>
2022-07-24 19:09:59 +0200

    Added simple config tab

--- gui.c
+++ gui.c
@@ -1,4 +1,8 @@
+#include "config.h"
+
 #include "gui.h"
+
+static int currentTab = 0;
 
 bool gui_isOpen = 1;
 
@@ -12,18 +16,35 @@ void gui_handleToggle(struct nk_context *ctx)
 	}
 }
 
+static void renderConfigTab(struct nk_context *ctx)
+{
+	nk_layout_row_dynamic(ctx, 500, 1);
+	if (nk_group_begin(ctx, "Config", NK_WINDOW_BORDER | NK_WINDOW_NO_SCROLLBAR | NK_WINDOW_DYNAMIC)) {
+		nk_layout_row_dynamic(ctx, 25, 1);
+
+		static char buf[256];
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, buf, sizeof(buf), nk_filter_ascii);
+
+		if (nk_button_label(ctx, "Load"))
+			config_load(buf);
+
+		nk_group_end(ctx);
+	}
+}
+
 void gui_render(struct nk_context *ctx, SDL_Window *window)
 {
 	int flags = NK_WINDOW_BORDER | NK_WINDOW_TITLE | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_NO_SCROLLBAR;
 
-	if (nk_begin(ctx, "ah4", nk_rect(0, 0, 200, 200), flags)) {
-		/*
-                nk_layout_row_dynamic(ctx, 30, 3);
+	if (nk_begin(ctx, "ah4", nk_rect(0, 0, 600, 500), flags)) {
+		nk_layout_row_dynamic(ctx, 50, 1);
 
-		nk_button_label(ctx, "test");
-		nk_button_label(ctx, "test");
-		nk_button_label(ctx, "test");
-		*/
+		if (nk_button_label(ctx, "Config"))
+			currentTab = 0;
+
+		switch (currentTab) {
+		case 0: renderConfigTab(ctx);
+		}
 
 		nk_end(ctx);
 	}
