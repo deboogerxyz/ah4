58c70079f0a56df348795f93a6c01017cb15d756
debooger <debooger@debooger.xyz>
2022-07-28 12:17:23 +0200

    Use -Og instead of -O0 when compiling for debugging
    Some compiler passes that collect debug information are disabled at -O0.
    -Og is optimized for debugging.

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ LIBS = -lpthread -lcjson -lGLEW -lSDL2
 
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L
-#CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O0 -fPIC ${CPPFLAGS} -fsanitize=undefined
+#CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -Og -fPIC ${CPPFLAGS} -fsanitize=undefined
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O3 -fPIC ${CPPFLAGS}
 LDFLAGS  = -shared ${LIBS}
 
