33c694d9567cce230d0b134ac7511c9bc1d97bcc
debooger <debooger@debooger.xyz>
2022-07-30 20:15:56 +0200

    Disabled LTO
    It slows the compilation and is not worth it.

--- config.mk
+++ config.mk
@@ -4,8 +4,8 @@ LIBS = -lpthread -lcjson -lGLEW -lSDL2
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L
 #CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O0 -fPIC ${CPPFLAGS} -fsanitize=undefined
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O3 -fPIC -flto=auto ${CPPFLAGS}
-LDFLAGS  = -shared -flto=auto ${LIBS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O3 -fPIC ${CPPFLAGS}
+LDFLAGS  = -shared ${LIBS}
 
 # Compiler and linker
 CC = cc
