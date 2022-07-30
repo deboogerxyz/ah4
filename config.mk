3ec8ecbd918bc5ffb510aabb274636b443ecf60f
debooger <debooger@debooger.xyz>
2022-07-30 20:16:24 +0200

    Compile with -O2 instead of -O3

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ LIBS = -lpthread -lcjson -lGLEW -lSDL2
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L
 #CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O0 -fPIC ${CPPFLAGS} -fsanitize=undefined
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O3 -fPIC ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O2 -fPIC ${CPPFLAGS}
 LDFLAGS  = -shared ${LIBS}
 
 # Compiler and linker
