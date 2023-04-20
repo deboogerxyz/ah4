f38010477fd5d17e0dfddf9e9107af498a7a5421
debooger <debooga@gmail.com>
2023-04-20 11:47:32 +0200

    Use more aggresive compiler optimizations

--- config.mk
+++ config.mk
@@ -3,8 +3,8 @@ LIBS = -lpthread -lcjson -lGLEW -lSDL2
 
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L
-#CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O0 -fPIC ${CPPFLAGS} -fsanitize=undefined
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O2 -fPIC ${CPPFLAGS}
+#CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -O0 -fPIC -march=native ${CPPFLAGS} -fsanitize=undefined
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Wno-pointer-arith -Ofast -march=native -fPIC ${CPPFLAGS}
 LDFLAGS  = -shared ${LIBS}
 
 # Compiler and linker
